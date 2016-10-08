c11d3582514bebe7c594f8510527582221879eba
SkylakeCoder <skylakecoder@gmail.com>
2016-10-08 17:40:29 +0800

    remove test code (Test.js).

--- src/static/js/Test.js
+++
@@ -1,8 +0,0 @@
-Test = {}
-
-Test.Get = function() {
-    console.log("get...");
-    $.get("http://localhost:8686", function(data, status) {
-        console.log("data:" + data + ", status:" + status)
-    });
-}
